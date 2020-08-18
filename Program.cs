b9a0190f08a8a423b8ab9b56afd6377e5adde0b4
cJaydin <67625558+cJaydin@users.noreply.github.com>
2020-08-18 10:56:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsAndClasses
+{
+    class Program
+    {
+        class Fairy
+        {
+
+        }
+
+        class Corgi
+        {
+            public string name;
+            public static bool winner = true; // class variable
+            private int width, height, length;
+            private Fairy rider;
+
+            public string Borf()
+            {
+                return name + ": Bark!";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Corgi clara = new Corgi();
+            Corgi basil = new Corgi();
+
+            clara.name = "clara";
+            basil.name = "basil";
+
+            Console.WriteLine(clara.Borf());
+            Console.WriteLine(basil.Borf());
+        }
+    }
+}
